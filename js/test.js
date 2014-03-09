face85d6954d30202e41c5e7c743a871b947e831
Axsusia <zion437@gmail.com>
2014-03-09 16:14:20 +0900

    no message

--- js/test.js
+++ js/test.js
@@ -1,7 +1,21 @@
-function FirstCtrl($scope){
+console.log('for test  <<<<<');
+var myApp = angular.module('myApp',[]);
+
+myApp.factory('Data', function (){
+	return {message : 'this is from js data'};
+});
+
+function FirstCtrl($scope, Data){
 	//$scope = {message : "   ssw test"}
+	$scope.data = Data;
 }
 
-function SecondCtrl($scope){
-	// second scop
+function SecondCtrl($scope, Data){
+	// second scope
+	$scope.data = Data;
+
+
+	$scope.reversedMessage = function(message){
+		return message.split("").reverse().join("");
+	}
 }
